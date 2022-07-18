da8a01981785970ebebf94e77a0b2e6c71ce7970
harry101491 <hp1014@nyu.edu>
2022-07-18 11:57:25 -0700

    feat(hoisting exercize and hoisting lesson)

--- tasks/JsConcepts/Hoisting.js
+++ tasks/JsConcepts/Hoisting.js
@@ -15,3 +15,17 @@ function otherTeacher() {
   console.log(teacher); // undefined because function is getting hoisted at the top and teacher is undefined
   var teacher = 'suzy';
 }
+
+{
+  teacher = 'Kyle'; // Temporal dead zone error TDZ error
+  let teacher;
+}
+
+var teacher = 'Kyle';
+
+// var hoist and initialize the value with undefined
+// but let and const hoist without initializing and then gives TDZ error
+{
+  console.log(teacher); // tdz error
+  const teacher = 'Kyle';
+}
